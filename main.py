dce6f7d6123e0e0312d036324420dd736a8dc1d1
samuel-2004 <105411500+samuel-2004@users.noreply.github.com>
2022-12-18 11:39:04 +0800

    Changed to slash commands

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 import discord
 from discord import app_commands
+
 from defcombos import convertrawdecklist, shortencard, scrapesheet, getnumcombos, getlistofcombos, getcomboidsfromlist
 from createembed import createembed
 from getdecklistfromurl import decipherurl
 from salt import getsaltlist, gettaglist, addsaltmsg, addtagmsg
 from random import randint
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 from discord.ext import tasks
 from assist import convertstrtoint
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
+
 f = open("TOKEN.txt")
 TOKEN = f.read()
 f.close()
+
 scrapesheet()
 combos = getlistofcombos()
 
 col = 0xfc0fc0
 
-global saltlist, taglist, oldsaltindex, oldtagindex
 taglist = gettaglist()
 oldtagindex = len(taglist)
 saltlist = getsaltlist()
 oldsaltindex = len(saltlist)
+
 def getnext(old, issalt):
+    global saltlist, taglist, oldsaltindex, oldtagindex
     old += 1
     if issalt:
         if old >= len(saltlist):
@@ -32,7 +38,7 @@ def getnext(old, issalt):
             saltlist = getsaltlist()
             return saltlist[0]
         oldsaltindex = old
-        return saltlist[old]
+        return saltlist[oldsaltindex]
     else:
         if old >= len(taglist):
             oldtagindex = 0
@@ -45,13 +51,15 @@ def getnext(old, issalt):
 
 @client.event
 async def on_ready():
+    print("Syncing...")
+    await tree.sync(guild=discord.Object(id=632440454847135745))
     the_name = str(getnumcombos()) + ' combos at once'
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Game(name=the_name))
     update_info.start()
 
-@tree.command(name="sbhelp", description="Returns the help message")
-async def first_command(interaction):
+@tree.command(name="sbhelp", description="Returns the help message", guild=discord.Object(id=632440454847135745))
+async def sbhelp(interaction):
     helpm = """
 **!sbhelp** Shows this help message.
 **!sbid <id>** Shows the combo with the given id.
@@ -59,14 +67,14 @@ async def first_command(interaction):
 ~~**!sbcolor <wubrgc>** Shows a list of combos which match the color identity.~~
 ~~**!sbresult <result>** Shows a list of combos which match the results.~~
 **!sbrandom** Displays a random combo.
-**!combocheck** or **!cc** Upload a text file with your decklist or a link to your deck and the bot will let you know what combos are in your deck!
+**!combocheck** Upload a text file with your decklist or a link to your deck and the bot will let you know what combos are in your deck!
 ~~**!combo** This command is a way to search the site without ever leaving Discord! Handy!~~
 **!salt** Have you ever been salty? Well this bot has too, and it will certainly tell you so."""
     embed = discord.Embed(title="**List of Available Commands:**", description=helpm, color=col)
-    await interaction.response.send(embed=embed)
+    await interaction.response.send_message(embed=embed)
 
-@tree.command(name="cchelp", description="Returns the help message for how the bot checks combos in a decklist")
-async def first_command(interaction):
+@tree.command(name="cchelp", description="Returns the help message for checking a decklist", guild=discord.Object(id=632440454847135745))
+async def cchelp(interaction):
     helpm = """
 Most deck builder websites have an *export* or a *download* feature. You can drag the file ending with *.txt* onto discord and type **!cc** and press enter, and the bot will give you a list of combos.
 If you don't want to download the deck, you can instead provide a link to the decklist.
@@ -75,13 +83,77 @@ Supported websites currently include Aetherhub, Archidekt, Deckbox, Deckstats, M
 If you have any other websites you would like to see added to this list, please feel free to message <@579345356450693120>.
 """
     embed = discord.Embed(title="**Combo Check Help:**", description=helpm, color=col)
-    await interaction.response.send(embed=embed)
+    await interaction.response.send_message(embed=embed)
+
+@tree.command(name="combocheck", description="Returns the help message for checking a decklist", guild=discord.Object(id=632440454847135745))
+async def combocheck(interaction, decklist: discord.Attachment=None, decklist_url: str=None):
+    #fixing variable names
+    attachment = decklist
+    url = decklist_url
+    
+    rawdata = ""
+    cardslst = []
+    if attachment != None:
+        url = attachment.url
+
+        req = Request(
+            url=url, 
+            headers={'User-Agent': 'Mozilla/5.0'}
+        )
+        rawdata = urlopen(req).read().decode('utf-8')
+    elif url != None:
+        cardslst = decipherurl(url)
+        
+    if rawdata == "" and len(cardslst) == 0:
+        await interaction.response.send_message('No decklist was attached!')
+        return
+    if len(cardslst) == 0:
+        cardslst = convertrawdecklist(rawdata)
+    comboids = getcomboidsfromlist(cardslst)
+    if len(comboids) == 0:
+        await interaction.response.send_message('No combos found')
+        return
+    e = createembed(comboids[0],combos)
+    if len(comboids) > 1 and len(comboids) < 7:
+        s = str(len(comboids)) + ' more combos, please view combos '
+        for c in comboids:
+            s+= '[**' + str(c) + '**](https://commanderspellbook.com/combo/'+str(c)+'/), '
+        s = s[:-2] + "."
+        e.add_field(name='\u200b', value=s, inline=False)
+    elif len(comboids) != 1:
+        s = str(len(comboids) - 1) + ' more combos not listed here, please view combos '
+        for c in comboids[1:]:
+            s += str(c) + ", "
+        s = s[:-2] + "."
+        e.add_field(name='\u200b', value=s, inline=False)
+    await interaction.response.send_message(embed=e)
+
+@tree.command(name="sbrandom", description="Displays a random combo", guild=discord.Object(id=632440454847135745))
+async def sbrandom(interaction):
+    m = randint(1, getnumcombos())
+    embed=createembed(m,combos)
+    await interaction.response.send_message(embed=embed)
+    
+@tree.command(name="sbid", description="Displays the combo with the given id", guild=discord.Object(id=632440454847135745))
+async def sbid(interaction, identity: int):
+    m = identity
+    try:
+        embed=createembed(m,combos)
+        await interaction.response.send_message(embed=embed)
+    except:
+        interaction.response.send_message("Invalid identity!")
+
+@tree.command(name="salt", description="Have you ever been salty? Well this bot has too, and it will certainly tell you so.", guild=discord.Object(id=632440454847135745))
+async def salt(interaction):
+    outstring = getnext(oldsaltindex, True)
+    await interaction.response.send_message(outstring)
 
 @client.event
 async def on_message(msg):
     message = msg.content.lower()
     if msg.author.bot: # message was sent by any bot
         return
+
     if client.user.mentioned_in(msg):
         outstring = getnext(oldtagindex, False)
         await msg.channel.send(outstring)
@@ -90,56 +162,6 @@ async def on_message(msg):
         addsaltmsg(msg.content.replace("!saltadd","").strip())
     elif message.startswith("!tagadd"): # add a temporary message to the tag shortlist
         addtagmsg(msg.content.replace("!tagadd","").strip())
-    elif message.startswith("!salt"): #send a random salt message
-        outstring = getnext(oldsaltindex, True)
-        await msg.channel.send(outstring)
-    
-    elif message.startswith("!sbid "): #return embed containing the combo with the id passed by the user
-        m = msg.content.replace("!sbid ","")
-        try:
-            m = convertstrtoint(m)
-            await msg.channel.send(embed=createembed(m,combos))
-        except:
-            await msg.channel.send("Invalid identity!")
-            raise
-    elif message.startswith("!sbrand"): # returns an embed containing a random combo
-        ran = randint(1, getnumcombos())
-        await msg.channel.send(embed=createembed(ran,combos))
-    elif message.startswith("!cc") or message.startswith("!combocheck"):
-        rawdata = ""
-        cardslst = []
-        if len(msg.attachments) == 0:
-            if "```" in msg.content:
-                rawdata = msg.content.split("```")[1]
-            elif "http" in message:
-                url = msg.content.split(' ')[1] #should ignore the !cc prefix
-                cardslst = decipherurl(url)
-            else:
-                return await msg.channel.send('No decklist was attached!')
-        else:
-            attachment_url = message.attachments[0].url
-            rawdata = urlopen(attachment_url).read()
-        if rawdata == "" and len(cardslst) == 0:
-            return await msg.channel.send('No decklist was attached!')
-        if len(cardslst) == 0:
-            cardslst = convertrawdecklist(rawdata)
-        comboids = getcomboidsfromlist(cardslst)
-        if len(comboids) == 0:
-            return await msg.channel.send('No combos found')
-        e = createembed(comboids[0],combos)
-        if len(comboids) > 1 and len(comboids) < 7:
-            s = str(len(comboids)) + ' more combos, please view combos '
-            for c in comboids:
-                s+= '[**' + str(c) + '**](https://commanderspellbook.com/combo/'+str(c)+'/), '
-            s = s[:-2] + "."
-            e.add_field(name='\u200b', value=s, inline=False)
-        elif len(comboids) != 1:
-            s = str(len(comboids) - 1) + ' more combos not listed here, please view combos '
-            for c in comboids:
-                s += str(c) + ", "
-            s = s[:-2] + "."
-            e.add_field(name='\u200b', value=s, inline=False)
-        return await msg.channel.send(embed=e)
         
         
 @tasks.loop(hours=1) # this function updates the startup info every hour, including all sheets num combos and status
